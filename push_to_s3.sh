5038d03c556365c16b5d879c99014f7b69fbdfbb
praneeth.k <praneeth.k@darwinbox.io>
2024-08-17 01:08:19 +0530

    script changes

--- push_to_s3.sh
+++ push_to_s3.sh
@@ -5,9 +5,8 @@
 S3_BUCKET=$1
 BRANCH_NAME=$2
 COMMIT_ID=$3
-CLONE_DIR="."
+CLONE_DIR=$4  # Directory where the repository is cloned, e.g., 'qa'
 
-
-# Sync the local directory to the S3 destination
+# Sync the contents of the specified directory to the S3 bucket
 aws s3 sync $CLONE_DIR s3://$S3_BUCKET/$BRANCH_NAME/$COMMIT_ID/ --delete
 echo "All files have been uploaded to S3 under ${BRANCH_NAME}-${COMMIT_ID}/"
