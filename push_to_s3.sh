fc67da11a2e70ad690f0d6db7559ebc839f5646f
praneethkusuma <85992673+praneethkusuma@users.noreply.github.com>
2024-10-01 01:34:36 +0530

    Update push_to_s3.sh

--- push_to_s3.sh
+++ push_to_s3.sh
@@ -4,12 +4,12 @@
 
 S3_BUCKET=$1
 BRANCH_NAME=$2
-#COMMIT_ID=$3
+COMMIT_ID=$4
 TARGETDIR=$3  # Directory where the repository is cloned, e.g., 'qa'
 
 # Sync the contents of the specified directory to the S3 bucket
 #aws s3 sync $CLONE_DIR s3://$S3_BUCKET/$BRANCH_NAME/$COMMIT_ID/ --delete
 echo "TARGETDIR: $TARGETDIR"
-aws s3 cp $TARGETDIR s3://$S3_BUCKET/$BRANCH_NAME/ --recursive
-echo "aws s3 cp $TARGETDIR s3://$S3_BUCKET/$BRANCH_NAME/ --recursive"
+aws s3 cp $TARGETDIR s3://$S3_BUCKET/$BRANCH_NAME-$COMMIT_ID/ --recursive
+echo "aws s3 cp $TARGETDIR s3://$S3_BUCKET/$BRANCH_NAME-$COMMIT_ID/ --recursive"
 echo "All files have been uploaded to S3 under ${BRANCH_NAME}/"
